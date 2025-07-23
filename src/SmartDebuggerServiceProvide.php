f1ddf3cca16898ba9fe969ea65fe10bafa83c4fd
Sajib <ahmedsajib1999s@gmail.com>
2025-07-23 11:50:23 +0600

    Update composer.json and service provider, remove vendor from commit

--- src/SmartDebuggerServiceProvide.php
+++ src/SmartDebuggerServiceProvide.php
@@ -6,24 +6,42 @@ use Illuminate\Support\ServiceProvider;
 
 class SmartDebuggerServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-        $this->mergeConfigFrom(__DIR__.'/../config/smart-debugger.php', 'smart-debugger');
-    }
-
     public function boot()
     {
-        // Publish config & migration
-        $this->publishes([
-            __DIR__.'/../config/smart-debugger.php' => config_path('smart-debugger.php'),
-        ], 'smart-debugger');
+        $laravelVersion = app()->version();
 
-        $this->publishes([
-            __DIR__.'/../database/migrations/' => database_path('migrations'),
-        ], 'smart-debugger');
+        // Laravel version অনুযায়ী route ফাইল লোড করা
+        if (version_compare($laravelVersion, '11.0.0', '<')) {
+            // Laravel 9 এবং 10 এর জন্য
+            $this->loadRoutesFrom(__DIR__ . '/routes_v9_to_10.php');
+        } else {
+            // Laravel 11 এবং 12 এর জন্য
+            $this->loadRoutesFrom(__DIR__ . '/routes_v11_plus.php');
+        }
 
-        // Load routes & views
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'smart-debugger');
+        // Config publish
+        $this->publishes([
+            __DIR__.'/config/smartdebugger.php' => config_path('smartdebugger.php'),
+        ], 'config');
+
+        // View publish (যদি থাকে)
+        $this->publishes([
+            __DIR__.'/resources/views' => resource_path('views/vendor/smartdebugger'),
+        ], 'views');
+
+        // Lang publish (যদি থাকে)
+        $this->publishes([
+            __DIR__.'/resources/lang' => resource_path('lang/vendor/smartdebugger'),
+        ], 'lang');
+
+        // অন্য যেকোন initialization এখানে দিবে
+    }
+
+    public function register()
+    {
+        // Configuration merge করতে পারো এখানে
+        $this->mergeConfigFrom(
+            __DIR__.'/config/smartdebugger.php', 'smartdebugger'
+        );
     }
 }
