cca012e1588041bb0f2d508747646c482cf69326
Sajib <ahmedsajib1999s@gmail.com>
2025-07-26 10:59:27 +0600

    Update smart debugger package features

--- src/SmartDebuggerServiceProvider.php
+++ src/SmartDebuggerServiceProvider.php
@@ -6,24 +6,36 @@ use Illuminate\Support\ServiceProvider;
 
 class SmartDebuggerServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap the application services.
+     */
     public function boot()
     {
+        // Publish config file
         $this->publishes([
             __DIR__.'/../config/smart-debugger.php' => config_path('smart-debugger.php'),
         ], 'config');
 
+        // Publish migration files
         $this->publishes([
             __DIR__.'/../database/migrations/' => database_path('migrations'),
         ], 'migrations');
 
+        // Publish views
         $this->publishes([
             __DIR__.'/../resources/views' => resource_path('views/vendor/smart-debugger'),
         ], 'views');
 
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        // Load routes
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+
+        // Load views
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'smart-debugger');
+
+        // Load migrations
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
+        // Register commands if running in console
         if ($this->app->runningInConsole()) {
             $this->commands([
                 Console\AnalyzeErrorsCommand::class,
@@ -34,8 +46,12 @@ class SmartDebuggerServiceProvider extends ServiceProvider
         }
     }
 
+    /**
+     * Register the application services.
+     */
     public function register()
     {
+        // Merge config file so user can override
         $this->mergeConfigFrom(
             __DIR__.'/../config/smart-debugger.php',
             'smart-debugger'
